449e57e5d85c64001dfd9625134b9761ac6df364
Ariel Tyson <ariel30190@hotmail.com>
2024-01-09 03:18:08 -0800

    Create unit test for GroupAnagrams Leetcode problem

---
+++ Tests/GroupAnagramsTest.java
@@ -0,0 +1,28 @@
+package Tests;
+
+import Problems.GroupAnagrams;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GroupAnagramsTest {
+    @Test
+    public void testGroupAnagrams() {
+        GroupAnagrams ga = new GroupAnagrams();
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> expected = Arrays.asList(
+                Arrays.asList("eat", "tea", "ate"),
+                Arrays.asList("tan", "nat"),
+                List.of("bat")
+        );
+        List<List<String>> actual = ga.groupAnagrams(strs);
+        assertEquals(expected.size(), actual.size());
+        for (List<String> group : actual) {
+            assertTrue(expected.contains(group));
+        }
+    }
+}
