aecb4f4c206b308e9f31056845bb2f70f2486154
Ariel Tyson <ariel30190@hotmail.com>
2024-01-11 05:41:46 -0800

    Create coressponding unit test for Contains Duplicate problem

---
+++ Tests/ContainsDuplicateTest.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import Problems.ContainsDuplicate;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContainsDuplicateTest {
+    @Test
+    public void testContainsDuplicate() {
+        ContainsDuplicate test = new ContainsDuplicate();
+
+        int[] nums1 = {1, 2, 3, 1};
+        assertTrue(test.containsDuplicate(nums1), "Array contains duplicates");
+
+        int[] nums2 = {1, 2, 3, 4};
+        assertFalse(test.containsDuplicate(nums2), "Array does not contain duplicates");
+
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        assertTrue(test.containsDuplicate(nums3), "Array contains duplicates");
+    }
+}
+
