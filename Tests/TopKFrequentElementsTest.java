7ab02c82141487ab26a3ba5478c2ad4824ed4221
Ariel Tyson <ariel30190@hotmail.com>
2024-01-15 08:29:31 -0800

    Refactored solution to sort both expected and actual arrays so the order of the solution would not matter

--- Tests/TopKFrequentElementsTest.java
+++ Tests/TopKFrequentElementsTest.java
@@ -2,6 +2,9 @@ package Tests;
 
 import Problems.TopKFrequentElements;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TopKFrequentElementsTest {
@@ -13,13 +16,17 @@ class TopKFrequentElementsTest {
         int[] nums1 = {1,1,1,2,2,3};
         int k1 = 2;
         int[] expected1 = {1,2};
-        assertArrayEquals(expected1, solution.topKFrequent(nums1, k1));
+        int[] result1 = solution.topKFrequent(nums1, k1);
+        Arrays.sort(expected1);
+        Arrays.sort(result1);
+        assertArrayEquals(expected1, result1);
 
         int[] nums2 = {1};
         int k2 = 1;
         int[] expected2 = {1};
-        assertArrayEquals(expected2, solution.topKFrequent(nums2, k2));
+        int[] result2 = solution.topKFrequent(nums2, k2);
+        Arrays.sort(expected2);
+        Arrays.sort(result2);
+        assertArrayEquals(expected2, result2);
     }
 }
-
-
