8eaf2dbf78ea93085321a09aa3ee21f3c11b1c1a
Ariel Tyson <ariel30190@hotmail.com>
2024-01-30 00:12:02 -0800

    Create definition for singly linked list with associated methods for testing

---
+++ LinkedList/ListNode.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+import java.util.Objects;
+
+public class ListNode {
+    int val;
+    public ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    // Method to convert an array to a linked list
+    public static ListNode arrayToList(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+        for (int i = 1; i < arr.length; i++) {
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode node = (ListNode) o;
+        return val == node.val && Objects.equals(next, node.next);
+    }
+
+}
