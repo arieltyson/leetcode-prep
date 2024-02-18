373b5e9ab47f50588de1d2ce6be190beeac88c00
Ariel Tyson <ariel30190@hotmail.com>
2024-02-18 00:55:27 -0400

    Solve Missing Number Leetcode probem

---
+++ BitManipulation/BitManipulationProblems/MissingNumber.java
@@ -0,0 +1,12 @@
+package BitManipulation.BitManipulationProblems;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int expectedSum = nums.length*(nums.length + 1)/2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+}
