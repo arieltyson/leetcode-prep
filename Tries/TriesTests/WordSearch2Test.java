bc841bc2762e7a13b34b43ed8703484a8dd0eda4
Ariel Tyson <ariel30190@hotmail.com>
2024-02-27 23:46:34 -0800

    Create corresponding JUnit unit test for Word Search II Leetcode problem

---
+++ Tries/TriesTests/WordSearch2Test.java
@@ -0,0 +1,24 @@
+package Tries.TriesTests;
+
+import Tries.TriesProblems.WordSearch2;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordSearch2Test {
+    @Test
+    public void testFindWords() {
+        WordSearch2 ws = new WordSearch2();
+        char[][] board = {
+                {'o','a','a','n'},
+                {'e','t','a','e'},
+                {'i','h','k','r'},
+                {'i','f','l','v'}
+        };
+        String[] words = {"oath","pea","eat","rain"};
+        List<String> expected = Arrays.asList("eat","oath");
+        List<String> result = ws.findWords(board, words);
+        assertEquals(expected, result);
+    }
+}
