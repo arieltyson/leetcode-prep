7fda17c7a631f57bc3609223b069f7f75f469dc5
Ariel Tyson <ariel30190@hotmail.com>
2024-03-27 00:04:16 -0700

    Rename files to fix package issues and edit incorrect test case in Longest Repeating Character Replacement problem

--- SlidingWindow/SlidingWindowTests/SlidingWindow/SlidingWindowTests/LongestRepeatingCharacterReplacementTest.java
+++ SlidingWindow/SlidingWindowTests/SlidingWindow/SlidingWindowTests/LongestRepeatingCharacterReplacementTest.java
@@ -1,4 +1,9 @@
+package SlidingWindow.SlidingWindowTests;
+
 import org.junit.jupiter.api.Test;
+
+import SlidingWindow.SlidingWindowProblems.LongestRepeatingCharacterReplacement;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LongestRepeatingCharacterReplacementTest {
@@ -11,7 +16,8 @@ public class LongestRepeatingCharacterReplacementTest {
         assertEquals(4, solution.characterReplacement("AABABBA", 1));
         assertEquals(2, solution.characterReplacement("AAB", 0));
         assertEquals(3, solution.characterReplacement("AAAB", 0));
-        assertEquals(5, solution.characterReplacement("AAABB", 1));
+        assertEquals(4, solution.characterReplacement("AAABB", 1)); // Updated this line
+        assertEquals(5, solution.characterReplacement("AAAAB", 1)); // Added this line
         assertEquals(0, solution.characterReplacement("", 1));
     }
 }
